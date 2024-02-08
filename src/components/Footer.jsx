2065b318dc0b3c1cf486f130411816c0c5cd45ab
Ahmed_Shtya <Shtya54@gmail.com>
2024-02-08 09:12:30 +0200

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,25 +21,26 @@ const Footer = memo(() => {
       <div className="container" >
 
       <div className="top">
-        <img src={Logo} alt="footer"  data-aos="fade-up" data-aos-delay="50" />
-        <div className="phead" data-aos="fade-up" data-aos-delay="100">{t("home.footer.h1")}</div>
+        <img src={Logo} alt="footer"  />
+
+        <div className="phead">{t("home.footer.h1")}</div>
         <ul className='social last-social'>
-          <li data-aos="fade-up" data-aos-delay="50">  <Link to={Social_Media[0]} target="_blank" className="Iicon"> <div className="style"> <i className="fa-brands fa-facebook-f"></i>    </div> </Link></li>
-          <li data-aos="fade-up" data-aos-delay="70"> <Link to={Social_Media[1]} target="_blank" className="Iicon"> <div className="style"> <i className="fa-brands fa-instagram"></i></div> </Link></li>
-          <li data-aos="fade-up" data-aos-delay="90"> <Link to={Social_Media[3]} target="_blank" className="Iicon"> <div className="style"><i className="fa-brands fa-whatsapp"></i>        </div> </Link></li>
-          <li data-aos="fade-up" data-aos-delay="110"> <Link to={Social_Media[2]} target="_blank" className="Iicon"> <div className="style"> <i className="fa-brands fa-linkedin-in"></i></div> </Link></li>
+          <li > <Link to={Social_Media[0]} target="_blank" className="Iicon"> <div className="style"> <i className="fa-brands fa-facebook-f"></i>    </div> </Link></li>
+          <li > <Link to={Social_Media[1]} target="_blank" className="Iicon"> <div className="style"> <i className="fa-brands fa-instagram"></i></div> </Link></li>
+          <li > <Link to={Social_Media[3]} target="_blank" className="Iicon"> <div className="style"><i className="fa-brands fa-whatsapp"></i>        </div> </Link></li>
+          <li > <Link to={Social_Media[2]} target="_blank" className="Iicon"> <div className="style"> <i className="fa-brands fa-linkedin-in"></i></div> </Link></li>
         </ul>
       </div>
 
       <div className="center">
-        <ul className='ul-1' data-aos="fade-up" data-aos-delay="120">
+        <ul className='ul-1' >
           <div className='h2 clip'> {t("home.footer.ul1.name")} </div>
           <li ><i className="fa-solid fa-envelope"></i>  info@mrmadvertisingdubai.com </li>
           <li ><i className="fa-solid fa-map-location-dot"></i>  Business Bay - Dubai </li>
           <li ><i className="fa-solid fa-phone"></i>  +971561384496 </li>
         </ul>
 
-        <ul className='ul-2' data-aos="fade-up" data-aos-delay="140">
+        <ul className='ul-2' >
           <div className='h2 clip'> {t("home.footer.ul2.name")} </div>
           <li > <Link to="/contact-us"> {t("home.footer.ul2.li1")}  </Link></li>
           <li > <Link to="/about-us"> {t("home.footer.ul2.li2")}  </Link></li>
@@ -47,7 +48,7 @@ const Footer = memo(() => {
           <li > <Link to="/blog"> {t("home.footer.ul2.li4")} </Link></li>
         </ul>
 
-        <ul className='ul-3' data-aos="fade-up" data-aos-delay="160" >
+        <ul className='ul-3' >
           <div className='h2 clip'> {t("home.footer.ul3.name")} </div>
           <li className='newsletter'>{t("home.footer.ul3.p")}</li>
           <div className="group">
@@ -59,7 +60,7 @@ const Footer = memo(() => {
 
       </div>
 
-        <div className="bottom" data-aos="fade-up" data-aos-delay="0" data-aos-offset="0">
+        <div className="bottom" >
           <p className='p' > {t("home.footer.ul4.name")} </p>
           <div className="last" >
             <Link className='p' to="/terms">  {t("home.footer.ul4.li1")} </Link>
